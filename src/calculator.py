23594d85ec300184638565086b58a8d8437994b7
Charles Wicklund <chwicklund@gmail.com>
2021-03-01 00:37:04 -0500

    Initial commit

---
+++ src/calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
---
+++ tests/test_Calculator.py
@@ -0,0 +1,21 @@
+import unittest
+
+from src.calculator.calculator import Calculator
+
+
+class CalcTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_result_is_zero_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
